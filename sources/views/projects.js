87e74a38c0c7de1443402b23ce44d07b03182934
Remi Kristelijn <rkristelijn@gmail.com>
2018-06-18 13:38:03 +0200

    fixed vulnerability stuff using gulp@next

--- sources/views/projects.js
+++ sources/views/projects.js
@@ -2,6 +2,6 @@ import {DHXView} from "dhx-optimus";
 
 export class ProjectsView extends DHXView{
 	render(){
-		this.ui = this.root.attachHTMLString("<div class='work_in_progress'>Main Dashboard2</div>");
+		this.ui = this.root.attachHTMLString("<div class='work_in_progress'>Main Dashboard</div>");
 	}
 }
