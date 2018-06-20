1302a3cf9dbb7d0c88340b16ba89778ef38d01e4
Remi Kristelijn <rkristelijn@gmail.com>
2018-06-20 11:48:33 +0200

    added diagram

--- sources/app.js
+++ sources/app.js
@@ -1,17 +1,17 @@
 import 'less/app.less';
 
-import {DHXApp} from 'dhx-optimus';
-import {TopView} from 'views/top.js';
+import { DHXApp } from 'dhx-optimus';
+import { TopView } from 'views/top.js';
 
 
-class MyApp extends DHXApp{
-	constructor(config){
-		super(config);
-		this.attachEvent('ToolbarClick', (id) => dhtmlx.alert(id + ' button was clicked'));
-	}
-	render(){
-		this.show(TopView);
-	}
+class MyApp extends DHXApp {
+  constructor(config) {
+    super(config);
+    this.attachEvent('ToolbarClick', (id) => dhtmlx.alert(id + ' button was clicked'));
+  }
+  render() {
+    this.show(TopView);
+  }
 }
 
 window.MyApp = MyApp;
